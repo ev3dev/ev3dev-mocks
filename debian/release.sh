13d19af0fdb2f17def413062785cbb7ca7906486
David Lechner <david@pybricks.com>
2019-06-27 23:00:03 -0500

    debian: revert temporary changes
    Fix some temporary commented out lines
    
    Fixes: 266ee83bfc1e8cb4cefc5a181b207ff5a630d4f6

--- debian/release.sh
+++ debian/release.sh
@@ -9,19 +9,19 @@ version=$(dpkg-parsechangelog -S Version)
 distribution=$(dpkg-parsechangelog -S Distribution)
 codename=$(debian-distro-info --codename --${distribution})
 
-# OS=debian DIST=${codename} ARCH=amd64 pbuilder-ev3dev build
+OS=debian DIST=${codename} ARCH=amd64 pbuilder-ev3dev build
 OS=debian DIST=${codename} ARCH=armhf PBUILDER_OPTIONS="--binary-arch" pbuilder-ev3dev build
 OS=debian DIST=${codename} ARCH=armel PBUILDER_OPTIONS="--binary-arch" pbuilder-ev3dev build
 OS=raspbian DIST=${codename} ARCH=armhf pbuilder-ev3dev build
 
-# debsign ~/pbuilder-ev3dev/debian/${codename}-amd64/${source}_${version}_amd64.changes
+debsign ~/pbuilder-ev3dev/debian/${codename}-amd64/${source}_${version}_amd64.changes
 debsign ~/pbuilder-ev3dev/debian/${codename}-armhf/${source}_${version}_armhf.changes
 debsign ~/pbuilder-ev3dev/debian/${codename}-armel/${source}_${version}_armel.changes
 debsign ~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version}_armhf.changes
 
-# dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-amd64/${source}_${version}_amd64.changes
+dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-amd64/${source}_${version}_amd64.changes
 dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-armhf/${source}_${version}_armhf.changes
 dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-armel/${source}_${version}_armel.changes
 dput ev3dev-raspbian ~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version}_armhf.changes
 
-# gbp buildpackage --git-tag-only
+gbp buildpackage --git-tag-only
